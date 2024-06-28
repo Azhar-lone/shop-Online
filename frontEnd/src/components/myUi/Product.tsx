c2eecb77f0d36b02d467780e0b11cf9bb7a96579
Azhar-lone <azharlonem@gmail.com>
2024-06-28 08:55:37 +0500

    0.9.3.3 commit

--- frontEnd/src/components/myUi/Product.tsx
+++ frontEnd/src/components/myUi/Product.tsx
@@ -12,7 +12,7 @@ const Product: React.FC<Type> = (
     const navigate = useNavigate()
     return (
         <div
-            className="lg:h-[60vh] h-96 mt-2 border rounded-2xl  shadow-2xl bg-card  sm:w-[48%] md:w-[31%] lg:w-[23%] w-[47%] hover:scale-105 hover:cursor-pointer delay-75 transition-all "
+            className="lg:h-[60vh] h-96 mt-2 border rounded-2xl  shadow-2xl bg-card  sm:w-[48%] md:w-[31%] lg:w-[23%] w-[47%] hover:scale-105 hover:cursor-pointer delay-75 transition-all animate-fade-in"
         >
             <img src={product.imgs[0]} alt="images"
                 className=" w-[100%] h-[80%] rounded-2xl"
